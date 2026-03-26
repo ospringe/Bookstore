1c4040a9520131b85aaa9035edf70a9645211107
Olivia Springer <oliviagspringer@gmail.com>
2026-03-26 13:33:58 -0600

    TBH IDK what these changes are

--- backend/Mission11Assignment.API/obj/Debug/net8.0/Mission11Assignment.API.AssemblyInfo.cs
+++ backend/Mission11Assignment.API/obj/Debug/net8.0/Mission11Assignment.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11Assignment.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10eb7acd57ce76da1d42df1ac34ea5e3debfd775")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04f0308b1550ca056cdc5afe14bbbb4a00088887")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11Assignment.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11Assignment.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
